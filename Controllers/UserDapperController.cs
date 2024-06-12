e75d62fffeee0786b5eba168a7d50404585edf7d
Ladyincodes <111862613+ladyincodes@users.noreply.github.com>
2024-06-12 23:05:58 +1200

    first commit

---
+++ Controllers/UserDapperController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserDapperController : Controller
+    {
+        [HttpGet(Name = "TestConnection")]
+        public DateTime TestConnection()
+        {
+            return DateTime.Now;
+        }
+    }
+}
