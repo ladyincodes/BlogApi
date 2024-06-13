952a3e6588cdf5699cabd21e14a3464fbaf2730e
Ladyincodes <111862613+ladyincodes@users.noreply.github.com>
2024-06-13 21:49:15 +1200

    Removed unrelated files, Added ConnectionString

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        public UserController(IConfiguration configuration)
+        {
+            Console.WriteLine(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        [HttpGet("TestConnection")]
+        public DateTime TestConnection()
+        {
+            return DateTime.Now;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// TODO: Add CORS policy here if needed
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
